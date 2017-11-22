1db113a765284083ef1836f308b0a65827d03f4d
Jorden Liu Chin Hao <jorden.liu1997@yahoo.com>
2017-11-22 22:04:31 +0800

    add code to get value of post, get, put and delete request

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express'),
     favicon = require('serve-favicon'),
     urlMain = require('./routes/main'),
-    path = require('path');
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser');
 
 app = express();
 app.set('view engine', 'pug');
@@ -12,6 +14,12 @@ app.set('views', './templates');
 // static
 app.use('/static', express.static('static'));
 
+// Middleware to parse POST request & cookies
+// Must include if you want to parse POST request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
 // use routes from routes/main.js
 app.use(urlMain);
 
@@ -19,6 +27,6 @@ app.use(favicon(path.join(__dirname, 'static', 'favicon', 'favicon.png')));
 
 const portNumber = 3000;
 app.listen(portNumber, function() {
-  console.log('listening on port ' + portNumber + '!');
-  console.log('http://localhost:' + portNumber);
+    console.log('listening on port ' + portNumber + '!');
+    console.log('http://localhost:' + portNumber);
 });
